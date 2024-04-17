f1a1bef14e7c158e23633e60427a08a68f8ac069
Hyrum <Hyrum.Jardine@outlook.com>
2024-04-16 23:59:54 -0600

    now updated

--- 323-labs-HJARDINE625-main/lab02/alu.sv
+++ 323-labs-HJARDINE625-main/lab02/alu.sv
@@ -20,28 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module alu(op1, op2, alu_op, zero, result);
+module alu(op1, op2, alu_op, result, zero);
     input wire logic [31:0] op1;
     input wire logic [31:0] op2;
     input wire logic [3:0] alu_op;
-    output logic zero;
     output logic [31:0] result;
+    output logic zero;
     
     
     // the following two signals are constants used to asses if the zero flag was raised
-    logic NOT_ZERO = 1;
-    logic YES_ZERO = 0;
+    localparam NOT_ZERO = 0;
+    localparam YES_ZERO = 1;
     
     //here are all the constants for the ALU operations
-	logic[3:0] ALUOP_AND = 4'b0000;
-	logic[3:0] ALUOP_OR = 4'b0001;
-	logic[3:0] ALUOP_ADD = 4'b0010;
-	logic[3:0] ALUOP_SUB = 4'b0110;
-	logic[3:0] ALUOP_LT = 4'b0111;
-	logic[3:0] ALUOP_SRL = 4'b1000;
-	logic[3:0] ALUOP_SLL = 4'b1001;
-	logic[3:0] ALUOP_SRA = 4'b1010;
-	logic[3:0] ALUOP_XOR = 4'b1101;
+	localparam[3:0] ALUOP_AND = 4'b0000;
+	localparam[3:0] ALUOP_OR = 4'b0001;
+	localparam[3:0] ALUOP_ADD = 4'b0010;
+	localparam[3:0] ALUOP_SUB = 4'b0110;
+	localparam[3:0] ALUOP_LT = 4'b0111;
+	localparam[3:0] ALUOP_SRL = 4'b1000;
+	localparam[3:0] ALUOP_SLL = 4'b1001;
+	localparam[3:0] ALUOP_SRA = 4'b1010;
+	localparam[3:0] ALUOP_XOR = 4'b1101;
 	
 	// the following code assigns all the result return value to the correct operation 
 	always_comb begin
@@ -63,13 +63,23 @@ module alu(op1, op2, alu_op, zero, result);
     
     // We also need this module just to make sure the zero flag is properly raised.
     always_comb begin
-    //first make sure we were working with the comparitior opperation
-        if (alu_op == ALUOP_LT)
-        //now see if it gave a zero or not
-            if(result == 32'd1)
-                zero = NOT_ZERO;
-                //if so raise the zero flag
-            else
-                zero = YES_ZERO;
+    //It seems like we always want to know if we got zero so
+        if(result == 32'd0)
+        //if so raise the zero flage
+            zero = YES_ZERO;
+        else
+        //it is not zero so
+            zero = NOT_ZERO;
+//    //first make sure we were working with the comparitior opperation
+//        if (alu_op == ALUOP_LT)
+//        //now see if it gave a zero or not
+//            if(result == 32'd1)
+//                zero = NOT_ZERO;
+//                //if so raise the zero flag
+//            else
+//                zero = YES_ZERO;
+//         //here we check if we need to have a zero for non-testing functions.
+//         else
+//            zero = YES_ZERO;
     end
 endmodule
